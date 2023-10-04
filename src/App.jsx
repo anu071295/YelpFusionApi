d1b777015392af83f25d9364824c906aa9f98bd0
anu071295 <nanuredha0795@gmail.com>
2023-10-04 17:47:46 -0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+import './App.css'
+import axios from 'axios';
+
+function App() {
+  const token = /**Add token */ '';
+  const apiURL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?sort_by=best_match&limit=20&location=Indiana';
+  const headers = {
+    "Authorization": `Bearer ${token}`,
+    "accept": "application/json",
+    "x-requested-with": "xmlhttprequest",
+    "Access-Control-Allow-Origin": "*",
+  }
+
+    axios
+    .get(apiURL, { headers })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error('Error fetching data from Yelp API:', error);
+    });
+  return (
+    <>
+      Hello
+    </>
+  )
+}
+
+export default App
+
+  /*const options = {method: 'GET', headers: {accept: 'application/json', Authorization: 'Bearer' + token},mode: 'no-cors'};
+
+  fetch('https://api.yelp.com/v3/businesses/search?sort_by=best_match&limit=20', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));*/
